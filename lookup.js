3657cca4b3fffa4762bb66af647b80cafb7ab4c2
rseroter <rseroter@hotmail.com>
2018-07-25 20:16:48 -0700

    initial commit

---
+++ lookup.js
@@ -0,0 +1,11 @@
+module.exports = function (customerid) {
+
+    console.log(customerid);
+
+    //TODO do database lookup
+    
+    //return static results
+    var customer = {id: customerid, name:'Richard', title:'phony developer'};
+
+    return customer;
+}
